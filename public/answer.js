a283bcc3fa05b6a273fdba8ccbf931602e489af0
Jeff88775 <54448801+Jeff88775@users.noreply.github.com>
2020-04-23 12:32:40 -0500

    Chartjs

--- public/answer.js
+++ public/answer.js
@@ -144,7 +144,46 @@ const appendQuestion = (question) => {
   updateCount();
 };
 
+function dibujar(respuestas){
+  var ctx = document.getElementById('myChart').getContext('2d');
+      var myChart = new Chart(ctx, {
+          type: 'line',
+          data: {
+              labels: ['Pregunta1', 'Pregunta2', 'Pregunta3', 'Pregunta4', 'Pregunta5'],
+              datasets: [{
+                  label: 'Preguntas Correctas',
+                  //data: [5, 2, 0, 1, 3],
+                  data: respuestas,
+                  backgroundColor: [
+                      'rgba(255, 99, 132, 0.2)',
+                      'rgba(54, 162, 235, 0.2)',
+                      'rgba(255, 206, 86, 0.2)',
+                      'rgba(75, 192, 192, 0.2)',
+                      'rgba(153, 102, 255, 0.2)',
+                  ],
+                  borderColor: [
+                      'rgba(255, 99, 132, 1)',
+                      'rgba(54, 162, 235, 1)',
+                      'rgba(255, 206, 86, 1)',
+                      'rgba(75, 192, 192, 1)',
+                      'rgba(153, 102, 255, 1)',
+                  ],
+                  borderWidth: 1
+              }]
+          },
+          options: {
+              scales: {
+                  yAxes: [{
+                      ticks: {
+                          beginAtZero: true
+                      }
+                  }]
+              }
+          }
+      });
+}
 function enviarTest() {
+  dibujar();
   console.log('submitted');
   total = document.getElementsByClassName('col-12 col-md-6 pl-2 pr-2 mb-4').length -1;
   
